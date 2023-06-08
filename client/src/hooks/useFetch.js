78fdea339de4de6a7873ecf205c73af862bbe7dc
henritresor <shimwamanahenritresor@gmail.com>
2023-06-08 21:43:12 +0200

    created a usefetch hook

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const useFetch = (uri) => {
+
+    const [data, setData] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState({
+        status: true,
+        error: null
+    })
+
+    const fetch = async () => {
+        setIsLoading(true)
+        try {
+            const res = await axios.get(uri);
+            setIsLoading(false)
+            setData(res.data)
+        } catch (error) {
+            setIsLoading(false)
+            setData(null)
+            setError({
+                status: true,
+                error
+            })
+        }
+    }
+    useEffect(() => {
+        let isCancelled = false
+        if (!isCancelled) {
+            fetch()
+        }
+        return () => isCancelled = true
+    }, [])
+    return {
+        data, isLoading, error
+    }
+}
+
+export default useFetch
