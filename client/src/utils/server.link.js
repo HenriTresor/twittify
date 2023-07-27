222a41528716dd8e516dce38c738d0d3e2232be2
henritresor <shimwamanahenritresor@gmail.com>
2023-07-27 20:52:06 +0200

    changed let to const

--- client/src/utils/server.link.js
+++ client/src/utils/server.link.js
@@ -1,4 +1,4 @@
-let serverLink = 'https://twittify-server-c00i.onrender.com'
+const serverLink = 'https://twittify-server-c00i.onrender.com'
 // let serverLink = 'http://localhost:4000'
 
 export default serverLink
