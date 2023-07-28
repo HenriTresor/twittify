cadaa539485089c17d8d25c54f57e09e26a318bd
henritresor <shimwamanahenritresor@gmail.com>
2023-07-28 21:37:16 +0200

    changed quotes to double ones

--- client/src/utils/server.link.js
+++ client/src/utils/server.link.js
@@ -1,4 +1,4 @@
-const serverLink = 'https://twittify-server-c00i.onrender.com'
+const serverLink = "https://twittify-server-c00i.onrender.com"
 // let serverLink = 'http://localhost:4000'
 
 export default serverLink
